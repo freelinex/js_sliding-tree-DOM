bb7458e6d4038422adb16cfad268cc39680f3ff6
freelinex <freelinex7@gmail.com>
2026-02-18 21:09:25 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const lists = document.querySelectorAll('.tree li');
+
+lists.forEach((list) => {
+  if (list.children.length > 0) {
+    const span = document.createElement('span');
+
+    span.innerText = list.firstChild.textContent.trim().replace('\n', '');
+    list.firstChild.replaceWith(span);
+  }
+});
+
+const switches = document.querySelectorAll('span');
+
+switches.forEach((item) => {
+  item.addEventListener('click', () => {
+    item.nextElementSibling.style.display =
+      item.nextElementSibling.style.display === 'none' ? 'block' : 'none';
+  });
+});
